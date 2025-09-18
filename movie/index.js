d07a07861e24c23ccb5ef8ca33bbb35afb1c15e2
Rupesh155 <rudhg35@gmail.com>
2025-09-18 10:42:50 +0530

    movieapies

---
+++ movie/index.js
@@ -0,0 +1,33 @@
+// https://api.tvmaze.com/search/shows?q=${searchText}`
+
+
+
+
+let btn=   document.querySelector("button")
+let inp=   document.querySelector("input")
+btn.addEventListener("click",()=>{
+    let searchText=inp.value
+fetch(`https://api.tvmaze.com/search/shows?q=${searchText}`).
+then((data)=>{
+    return data.json()
+
+}).then((res)=>{
+    console.log(res);
+    show(res)
+    
+})
+
+})
+
+
+
+
+function show(res){
+   let img=  document.querySelector("img")
+//    console.log(res[0].show.image.medium);
+  let link=   res[0].show.image.medium
+img.setAttribute("src",link)
+   
+
+}
+
